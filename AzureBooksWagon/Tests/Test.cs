878c6c8392f00bdbec86692e5c66777a21326dc4
sidthakur258 <sidthakur258@gmail.com>
2020-10-02 17:28:14 +0530

    DELETED:All test cases except login page

--- AzureBooksWagon/Tests/Test.cs
+++ AzureBooksWagon/Tests/Test.cs
@@ -14,36 +14,6 @@ namespace AzureBookWagon.Test
             login.BookwagonLogin();
             Assert.AreEqual(driver.Title, title);
         }
-        [Test, Order(2)]
-        public void HomePageTest()
-        {
-            HomePage home = new HomePage(driver);
-            home.SearchItem("cobain");
-            Assert.IsTrue(home.Validate().Displayed);
-        }
-
-        [Test, Order(3)]
-        public void PlaceOrderTest()
-        {
-            PlaceOrder order = new PlaceOrder(driver);
-            order.OrderBook();
-            Assert.AreEqual(driver.Url, placeOrder);
-        }
-
-        [Test, Order(4)]
-        public void CartCheckoutTest()
-
-        {
-            ShippingAddress cart = new ShippingAddress(driver);
-            cart.AddShippingDetails();
-        }
-
-        [Test, Order(5)]
-        public void LogoutTest()
-        {
-            Logout logout = new Logout(driver);
-            logout.LogOut();
-            Assert.AreEqual(driver.Url, logOut);
-        }
+       
     }
 }
